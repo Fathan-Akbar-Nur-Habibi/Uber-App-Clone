05b1a544470add1379690f6f11e76f2e2294ad39
Fathan-Akbar-Nur-Habibi <fathanakbarnh@gmail.com>
2025-03-03 22:00:26 +0700

    improve code

--- components/Home/CarListOptions.js
+++ components/Home/CarListOptions.js
@@ -3,6 +3,7 @@ import CarListItem from './CarListItem'
 
 function CarListOptions({distance}) {
     const [activeIndex, setActiveIndex]=useState();
+    const [selectedCar, setSelectedCar]=useState([]);
   return (
     <div className='mt-5 p-5 overflow-auto h-[250px]'>
         <h2 className='text-[22px] font-bold'>Recommended</h2>
@@ -10,11 +11,20 @@ function CarListOptions({distance}) {
             <div className={`cursor-pointer p-2 px-4 rounded-md
                 border-black
                 ${activeIndex==index ? `border-[3px]`:null}`}
-            onClick={()=>setActiveIndex(index)}
+            onClick={()=>{setActiveIndex(index);
+                setSelectedCar(item)}}
             >
                 <CarListItem car={item} distance={distance} />
             </div>
         ))}
+        
+        {selectedCar?.name ? <div className='flex justify-between fixed
+         bottom-5 bg-white p-3 shadow-xl rounded-lg w-full md:w-[30%] border-[1px] items-center'> 
+            <h2> Make Payment For </h2>
+            <button className='p-3 bg-black text-white
+             rouded-lg text-center'>Request {selectedCar.name}</button>
+        </div> : null}
+
     </div>
   )
 }
